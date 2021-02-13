efad1080321af8bbaca948043b809b7dc5bbd2b3
tselun <51496041+tselun@users.noreply.github.com>
2021-02-13 14:31:23 -0800

    added check function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import twitter
+import tweepy
 
 
 class TwitterBot:
@@ -75,7 +75,29 @@ class TwitterBot:
         if tweets not in TwitterBot.new_speak:
             tweets.replace(tweets)
 
+    def check(self, user):
+        # leave this for now 
+        check = True
+        consumer_key = "8LfgceWJmQWwmjVMLEZCp4fvn"
+        consumer_secret = "wNbCmgCbpboKBwLjbdiZr7Ugyw9bhLNVz5hYUUJU7nRVEfl5Y9"
+        access_token = "872684552-W1M5j1gPwLNf7ZPYFwYu49HjjkohsZBttINuzhmE"
+        access_token_secret = "i7hVgq7furWacz4VF9UF4VsFPap5FnoxmAtXvKBWcFs2h"
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+        tweets = api.user_timeline(screen_name=user, count=200, include_rts = False, tweet_mode = 'extended')
+
+        for info in tweets[:3]:
+            if(info not in self.new_speak):
+                print("new speak violation: ", info.full_text)
+                check = False
+        return check
 
 team = TwitterBot('ScraperBot5', 'Arshdeep16')
 team.login()
 team.censor()
+# checks if following new speak
+check = team.check("ScraperBot5")
+print(check)
