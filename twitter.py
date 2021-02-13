5e2ec00d833e920a65a0c4b76156b718ccb5a452
mattias <47797867+CodePerson2@users.noreply.github.com>
2021-02-13 13:26:34 -0800

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,7 +1,5 @@
-# importing/validating credentials
 import tweepy
 import time
-
 
 key = "8LfgceWJmQWwmjVMLEZCp4fvn"
 secret_key = "wNbCmgCbpboKBwLjbdiZr7Ugyw9bhLNVz5hYUUJU7nRVEfl5Y9"
@@ -12,3 +10,12 @@ AccessKey = "872684552-W1M5j1gPwLNf7ZPYFwYu49HjjkohsZBttINuzhmE"
 AccessKey_secret = "i7hVgq7furWacz4VF9UF4VsFPap5FnoxmAtXvKBWcFs2h"
 
 auth.set_access_token(AccessKey, AccessKey_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+print(user.screen_name)
+
+for follower in tweepy.Cursor(api.friends).items():
+    print(follower.name)
